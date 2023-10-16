9e96908cc51e068739e7b90a5d03da92265a3b56
kadench <kadench@byui.edu>
2023-10-16 15:41:21 -0600

    Added what I did over the weekend.

--- Program.cs
+++ Program.cs
@@ -1,11 +1,22 @@
-class Program {
-static void Main(string[] args){
-    Console.WriteLine("What is the Reference?");
-    string reference = Console.ReadLine(); 
-    Console.WriteLine("What is the scripture you are memorizing?");
-    string scripture = Console.ReadLine();
-    
+using System;
 
+class Program
+{
+    static void Main(string[] args)
+    {   
+        // Welcoming the user to the program
+        Console.WriteLine("Welcome to the scripture memorizer program!");
+        Console.WriteLine("Follow the instructions below: ");
+        Console.WriteLine();
+        // Asking the user for the scripture reference.
+        Console.Write("Enter the reference of the scripture you want to memorize (ex. '1 Nephi 2:15'): ");
+        string khReference = Console.ReadLine();
+        // Asking user for the scriptire passage.
+        Console.Write("Enter the scripture passage (ex. 'And my father dwelt in a tent.'): ");
+        string khPassage = Console.ReadLine();
+        // Ask the user for the number of words hidden each time they press enter.
+        Console.WriteLine("Specify the hidden word count for each memorization round (ex. '3'): ");
+        string khHiddenWordCount = Console.ReadLine();
+        
     }
 }
-
