0d629b25f2b607eddc4c6dcd9fd8d6193d3df452
CrumMan <122419541+CrumMan@users.noreply.github.com>
2023-10-16 12:11:07 -0600

    refrence and scripture variable created

--- Program.cs
+++ Program.cs
@@ -1,7 +1,11 @@
 class Program {
-static void Main(string[] args)
-    {
- 
+static void Main(string[] args){
+    Console.WriteLine("What is the Reference?");
+    string reference = Console.ReadLine(); 
+    Console.WriteLine("What is the scripture you are memorizing?");
+    string scripture = Console.ReadLine();
+    
+
     }
 }
 
