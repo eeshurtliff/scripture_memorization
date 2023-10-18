1dcb64f4e00a10d6619f48c935bf878d6bceb465
CrumMan <122419541+CrumMan@users.noreply.github.com>
2023-10-18 10:00:27 -0600

    word made as best I can atm

--- word.cs
+++ word.cs
@@ -1,5 +1,26 @@
 using System;
+using System.Diagnostics.Contracts;
+using System.Dynamic;
+using System.Runtime.CompilerServices;
+using System.Security.Authentication.ExtendedProtection;
 
 class Word {
+    string JTPgivenWord = wordname
+    private wordbool = JTPSet_isDisplayable(JTPgivenWord);
+    public JTPSet_isDisplayable(wordname){
+        if (JTPgivenWord==" ___ "){
+            bool boolword= true;
+        }
+        else {
+            bool boolword = false;
+        };
+        return boolword;
+    }
+    public ChangeWord(wordname, wordbool){
+        if (wordbool==false){
+            wordname=" ___ ";
+        }
+        return wordname
+    }
 
 }
