99a8ae80c21561253e944251f1269af0a5607ca2
eeshurtliff <eeshurtliff@gmail.com>
2023-10-17 23:08:35 -0600

    worked on memorizer

--- Memorizer.cs
+++ Memorizer.cs
@@ -1,5 +1,33 @@
 using System;
 
 class Memorizer {
+    int _eesNumToHide;
+    List<int> _eesHiddenIndexes = new List<int>();
+    Scripture eesScripture = new Scripture(string eesReference, string eesVerse );
+    public Memorizer(){
+        _eesNumToHide = 2;
+    }
+
+    public void EesUpdateVerse(_eesNumToHide){
+        Random eesRandom = new Random();
+        List<string> eesVerse = Scripture.GetVerse();
+        for (int i = 0; i < _eesNumToHide + 1; i++){
+            int eesHideIndex = eesRandom.Next(eesVerse.Count);
+            Word.Set_isDisplayable(false);
+            _eesHiddenIndexes.Add(eesHideIndex)
+        }
+    }
+
+    public void ToString(){
+        foreach (word in Scripture.ToString()){
+            Console.Write(Word.ToString());
+        }
+    }
     
 }
+
+
+
+// Research:
+// https://learn.microsoft.com/en-us/dotnet/api/system.random?view=net-7.0
+// stackoverflow.com/questions/1698308/finding-the-maximum-length-of-lists-in-c-sharp
--- word.cs
+++ word.cs
@@ -1,5 +1,5 @@
 using System;
 
-class Words {
+class Word {
 
 }
