5c694fd0251dec0c65c62d8743117c9628ff63f3
kadench <kadench@byui.edu>
2023-10-17 20:03:45 -0600

    Added setup for class method implementation.

--- Program.cs
+++ Program.cs
@@ -12,13 +12,24 @@ class Program {
         Console.Write("Enter the reference of the scripture you want to memorize (ex. '1 Nephi 2:15'): ");
         string khReference = Console.ReadLine();
 
+        // Create an instance of reference.
+        Reference khMainScriptureReference = new Reference();
+        
+        // Pass information to finished reference method here.
+
         // Asking user for the scriptire passage.
         Console.Write("Enter the scripture passage (ex. 'And my father dwelt in a tent.'): ");
         string khPassage = Console.ReadLine();
+
+        // Create an instance of scripture.
+        Scripture khMainScripture = new Scripture();
+        
+        // Pass information to finished scripture method here.
 
         // Ask the user for the number of words hidden each time they press enter.
         Console.Write("Specify the hidden word count for each memorization round. (ex. '3'): ");
         string khHiddenWordCount = Console.ReadLine();
 
+        // Pass information to finished Memorizer method here.
     }
 }
